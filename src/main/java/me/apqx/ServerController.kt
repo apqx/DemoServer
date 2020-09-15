38dc1d5797604b5215b4125abf0f4059ea97fcc4
apqx <changgongapq@gmail.com>
2020-09-16 02:07:21 +0800

    普通的GET与POST

--- src/main/java/me/apqx/ServerController.kt
+++ src/main/java/me/apqx/ServerController.kt
@@ -1,22 +1,38 @@
 package me.apqx
 
 import me.apqx.bean.Student
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class ServerController {
 
     /**
+     * GET
      * https://api.me.apqx.me/school?id=10
-     * 或者
-     * https://api.me.apqx.me/school?privateId=10
+     * defaultValue是当id不存在或未指定时，id的默认值
      */
     @GetMapping(value = ["/school"])
-    fun getStudent(@RequestParam(value = "id", defaultValue = "privateId") id: Long): Student {
+    fun getStudentByIdGET(@RequestParam(value = "id", defaultValue = "10") id: Long): Student {
         // 以Json的形式返回
         // TODO 如何返回一个自定义的字符串
         return Student(id, "Tom$id")
+    }
+
+    /**
+     * POST form-data表单
+     */
+    @PostMapping(value = ["/school"])
+    fun getStudentByIdPOST1(@RequestParam(value = "id", defaultValue = "10") id: Long): Student {
+        // 以Json的形式返回
+        // TODO 如何返回一个自定义的字符串
+        return Student(id, "Tom$id")
+    }
+
+    @PostMapping(value = ["/school"])
+    fun getStudentByIdPOST2(@RequestBody body: String): Student {
+        // 以Json的形式返回
+        println(body)
+        // TODO 如何返回一个自定义的字符串
+        return Student(1, "Tom")
     }
 }
